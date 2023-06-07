5cb1b71cb8773161d7f6996cc4163ca105293c8d
Jesus E Castillo <jesuseduaardo@gmail.com>
2023-06-07 00:18:56 -0300

    Se agrega title al logo para mostrar el nombre completo

--- src/Componentes/Logo/Logo.jsx
+++ src/Componentes/Logo/Logo.jsx
@@ -5,7 +5,7 @@ export const Logo = () => {
     <div className="contenedor">
       <div className="glass rotation sombra">
       </div>
-      <div className="logo-y-lema">
+      <div className="logo-y-lema" title="Con Que Vicio">
         <div className="logo resplandor">
           <span>C</span>
           <ImSearch className="rotation lupa"/>
