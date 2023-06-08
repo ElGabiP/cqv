bf60b7905a57a1b4e1dcd12bad6ebf1156c3d043
Monica Alejandra Gallardo <adav86@hotmail.com>
2023-06-07 22:04:09 -0300

    Agregamos footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import {Header} from "./Componentes/Header/Header";
-import { Inicio } from './Paginas/Inicio/Inicio';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Header } from "./Componentes/Header/Header";
+import { Inicio } from "./Paginas/Inicio/Inicio";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import { Footer } from "./Componentes/Footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path='/' element={<Inicio/>}></Route>
+        <Route path="/" element={<Inicio />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
-      
+
 export default App;
---
+++ src/Componentes/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../Navbar/Navbar";
+import "./Footer.css"
+
+export const Footer = () => {
+  return (
+    <footer className="d-flex d-md-none">
+      <Navbar />
+    </footer>
+  );
+};
