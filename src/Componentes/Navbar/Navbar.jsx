8416a2543c01a2dfdb6305b020cef098df3c5941
Monica Alejandra Gallardo <adav86@hotmail.com>
2023-06-08 22:26:15 -0300

    modificacion de detalles en Nav

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -7,19 +7,19 @@ import "./Navbar.css";
 export const Navbar = () => {
   return (
     <ul class="navbar-nav me-auto mb-2 mb-lg-0  ">
-      <li class="nav-item xxx">
+      <li class="nav-item icon">
         <a class="navegacion-icono" href="#">
           < BiCategory />
         </a>
         <p className= "fuente-principal regular">Generos</p>
       </li>
-      <li class="nav-item xxx">
+      <li class="nav-item icon">
         <a class="navegacion-icono" href="#">
           <FaGamepad />
         </a>
         <p className= "fuente-principal regular">Plataforma</p>
       </li>
-      <li class="nav-item xxx ">
+      <li class="nav-item icon ">
         <a class="navegacion-icono" href="#">
           <BsFillPersonFill />
         </a>
