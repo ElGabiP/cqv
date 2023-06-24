b3f932c626a2631070a0314dbb06088b7b32c6fd
Monica Alejandra Gallardo <104160939+Adav86@users.noreply.github.com>
2023-06-23 21:56:16 -0300

    Cambio carga logo (#14)
    Co-authored-by: Monica Alejandra Gallardo <adav86@hotmail.com>

--- src/Componentes/Carrusel/Carrusel.jsx
+++ src/Componentes/Carrusel/Carrusel.jsx
@@ -31,20 +31,5 @@ export const Carrusel = ({ sliders }) => {
         </button>
       </div>
     )
-  } else {
-    return (
-      <div className="cube-container">
-        <div className="cube-loader">
-          <div className="cube-top"></div>
-          <div className="cube-wrapper">
-            <span style={{ "--i": 0 }} className="cube-span"></span>
-            <span style={{ "--i": 1 }} className="cube-span"></span>
-            <span style={{ "--i": 2 }} className="cube-span"></span>
-            <span style={{ "--i": 3 }} className="cube-span"></span>
-          </div>
-          <p>Cargando</p>
-        </div>
-      </div>
-    )
   }
-}
+};
